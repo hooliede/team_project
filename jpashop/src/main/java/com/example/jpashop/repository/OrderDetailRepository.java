49e0fa49d66f2833b25384bf1b5a90b7b53779a8
hooliede <iojjdwn@gmail.com>
2025-03-01 23:39:31 +0900

    Delete jpashop directory

--- jpashop/src/main/java/com/example/jpashop/repository/OrderDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.jpashop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.jpashop.entity.OrderDetail;
-
-public interface OrderDetailRepository extends JpaRepository<OrderDetail, Integer> {
-
-}
--- jpashop/src/main/java/com/example/jpashop/repository/OrderItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.jpashop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.jpashop.entity.OrderItem;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {
-
-}
